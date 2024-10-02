3d3f9246ac9973a21689bd257e7c942748d69b8b
IrinaCol28 <irina.col2626@gmail.com>
2024-10-02 17:41:29 +0300

    Папки

---
+++ book1/test.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+# open(FILE, "<", "fred.txt") or die "Не удалось открыть файл: $!";
+# while (<FILE>) {
+#     chomp;
+#     if (/\b(?<word>\w*a)\b/) {  # Ищем слово, оканчивающееся на 'a'
+#         # print "Matched: '$1' in |$_|\n";  
+#         print "Matched: '$+{word}' in |$_|\n";  # $+{word} содержит слово, заканчивающееся на 'a'
+#     } else {
+#         print "No match: $_\n";
+#     }
+# }
+# close(FILE);
+
+my $m='dsgdsgd';
+ print "No match: $m \n";
+ print qw (No match: $m \n);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
